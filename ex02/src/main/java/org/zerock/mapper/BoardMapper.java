7fd7838bb035873822d9b82932173a0504e470ae
Joominseok <alstjr5911@naver.com>
2019-09-16 17:41:07 +0900

    인터페이스 mapper에는 메소드 선언후, mapper.xml 구현 @Test 작동여부 실행.

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,4 +10,8 @@ public interface BoardMapper {
 	// ->위에 @Select는 Boardmapper.xml을 사용하기 전에 사용했으므로 xml생성후에는 지워준것.
 	public List<BoardVO> getList();
 	
+	public void insert(BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -15,10 +16,23 @@ import lombok.extern.log4j.Log4j;
 public class BoardMapperTests {
 
 	@Setter(onMethod_ = @Autowired)
-	private BoardMapper mapper;
+	private BoardMapper mapper;  
+	
+//	@Test
+//	public void testGetList() {
+//		mapper.getList().forEach(board -> log.info(board));
+//	}
 	
 	@Test
-	public void testGetList() {
-		mapper.getList().forEach(board -> log.info(board));
+	public void testInsert() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		mapper.insert(board);
+		
+		log.info(board);
 	}
 }
